75251796d087cef8c7b26550015385ce92dfa7c0
Oleg Frolov <frolololeg@gmail.com>
2022-09-23 09:52:16 -0700

    removed Visual Scripting package

--- Assets/Scripts/FingerTipData.cs
+++ Assets/Scripts/FingerTipData.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Collections;
-using Unity.VisualScripting;
-using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
 
 [Serializable]
